ea2025b1a61697dc031e4769f313237e5461919e
MBorecki <borecki2@gmail.com>
2017-03-29 00:23:46 +0200

    mess with building

--- scripts/build.sh
+++ scripts/build.sh
@@ -17,10 +17,13 @@ files=(
 
 mkdir lib
 
+./node_modules/.bin/tsc -p ./tsconfig.json --declaration --outDir ./.publish-temp --declarationDir ./lib/typings
+
 for file in "${files[@]}"
 do
-    ./node_modules/.bin/tsc --declaration ./app/$file.ts --outDir ./.publish-temp --declarationDir ./lib/typings
     ./node_modules/.bin/browserify ./app/$file.ts -p tsify > ./lib/$file.js
 done
+
+# ./node_modules/.bin/dts-bundle --name input-action-binder --main ./lib/typings/index.d.ts --out ./../input-action-binder.d.ts
 
 rm -r "./.publish-temp"
